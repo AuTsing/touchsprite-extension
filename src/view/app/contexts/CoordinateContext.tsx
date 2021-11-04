ac1021d2ef0fe249298f246f7a6379800357a81f
roiff <roiff@126.com>
2021-11-05 02:06:50 +0800

    新增了取色器预览时,与中心颜色相同的颜色的提示框

--- src/view/app/contexts/CoordinateContext.tsx
+++ src/view/app/contexts/CoordinateContext.tsx
@@ -49,14 +49,39 @@ const CoordinateContextProvider = (props: { children: React.ReactNode }) => {
             }
             setX(x0);
             setY(y0);
-            setC(`0x` + `000000${activeJimp.getPixelColor(x0, y0).toString(16).slice(0, -2)}`.slice(-6));
-            new Jimp(21, 21, 0, (_, previewJimp) => {
+            const c0 = activeJimp.getPixelColor(x0, y0)
+            setC(`0x` + `000000${c0.toString(16).slice(0, -2)}`.slice(-6));
+            // 把图像大小改为21*14=294
+            new Jimp(294, 294, 0, (_, previewJimp) => {
                 for (let i = -10; i <= 10; ++i) {
                     for (let j = -10; j <= 10; ++j) {
                         const xx = i + x0;
                         const yy = j + y0;
                         if (xx >= 0 && xx < activeJimp.bitmap.width && yy >= 0 && yy < activeJimp.bitmap.height) {
-                            previewJimp.setPixelColor(activeJimp.getPixelColor(xx, yy), i + 10, j + 10);
+                            // 记录原图的点的颜色
+                            const cc = activeJimp.getPixelColor(xx, yy)
+                            // 每个放大的像素块为14*14的大小,遍历画图
+                            for (let ii = 0; ii <= 13; ++ii) {
+                                const x1 = (i + 10) * 14 + ii
+                                for (let jj = 0; jj <= 13; ++jj) {
+                                    const y1 = (j + 10) * 14 + jj
+                                    // 预留边框2个像素点,不是边框的填充原色
+                                    if (ii > 1 && ii < 12 && jj > 1 && jj < 12) {
+                                        previewJimp.setPixelColor(cc, x1, y1);
+                                    }else{
+                                        // 颜色如果和中心点一样,则画边框,边框2层,外层红色,内层白色
+                                        if (c0 == cc) {
+                                            if (ii == 0 || ii == 13 || jj == 0 || jj == 13) {
+                                                previewJimp.setPixelColor(0xFF0000FF, x1, y1);
+                                            }else{
+                                                previewJimp.setPixelColor(0xFFFFFFFF, x1, y1);
+                                            }
+                                        }else{
+                                            previewJimp.setPixelColor(cc, x1, y1);
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
                 }
